82179468fe304dd9beb2b47db6e61a64d6e1e038
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-06 14:38:42 -0300

    Rename

--- StarPatternPrograms/ReverseStairs.java
+++ StarPatternPrograms/ReverseStairs.java
@@ -1,2 +1,12 @@
 public class ReverseStairs {
+    public static void main(String[] args) {
+        int n = 0;
+        for (int i = 5; i > n; i--) {
+            for (int j = 0; j < i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
