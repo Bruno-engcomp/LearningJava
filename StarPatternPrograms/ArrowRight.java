b48315f19df1b612fdfcf4b5cb36c133c21caa2b
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-07 19:49:35 -0300

    Arrow Right Finished

--- StarPatternPrograms/ArrowRight.java
+++ StarPatternPrograms/ArrowRight.java
@@ -1,2 +1,17 @@
 public class ArrowRight {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < 5; i ++){
+            for (int j = 0; j <= i; j ++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 5; i > 0; i--){
+            for(int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
