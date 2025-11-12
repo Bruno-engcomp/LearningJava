ff1b4667efce0182984c2173f85ec6932431ca2c
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-12 13:50:51 -0300

    Number inverter

---
+++ Loops/NumberInverter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class NumberInverter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, original, inverter, rest;
+        System.out.print("Enter a number:");
+        n = input.nextInt();
+    }
+}
