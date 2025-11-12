42fea497be0cfb91176177e769f8631c3ca94bb8
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-12 16:49:47 -0300

    checkerBoard

---
+++ StarPatternPrograms/checkerBoard.java
@@ -0,0 +1,20 @@
+public class checkerBoard {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++)
+        {
+            if(i % 2 == 0) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print("* ");
+                }
+            }
+            else
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
