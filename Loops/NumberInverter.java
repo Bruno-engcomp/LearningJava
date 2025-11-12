87c56e96aa4671bf9e5c0a6ad829ba2674056b01
bruno.almd <brunoalmeida22007@gmail.com>
2025-11-12 13:54:07 -0300

    Number inverter finished

--- Loops/NumberInverter.java
+++ Loops/NumberInverter.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 public class NumberInverter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n, original, inverter, rest;
+        int n, original, inverted, rest;
+
         System.out.print("Enter a number:");
         n = input.nextInt();
+        original = n;
+        inverted = 0;
+
+        while(n > 0)
+        {
+            rest = n % 10;
+            inverted = inverted * 10 + rest;
+            n /= 10;
+        }
+        System.out.printf("The original number is %d and the inverted number is %d", original, inverted);
     }
 }
